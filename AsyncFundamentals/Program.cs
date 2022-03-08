9a1912b29ec75ed255acad9a81ee5d756fe9c2cc
roman-kart <romankartasheff@gmail.com>
2022-03-08 12:02:24 +0300

    TestAsync1

---
+++ AsyncFundamentals/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("tmp");
+TestAsyncFirst testAsyncFirst = new TestAsyncFirst();
+var a = testAsyncFirst.DelayResult<int>(12, new TimeSpan(0, 0, 2));
+
+Action<Task<int>> act = (x) =>
+{
+    var originalConsoleForeground = Console.ForegroundColor; 
+    if (!x.IsFaulted)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;    
+        Console.WriteLine("Task successfully complete!");
+        Console.WriteLine($"Result: {x.Result}");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Task has completed with error!");
+        Console.WriteLine($"Exception message: {x.Exception.Message}");
+    }
+    Console.ForegroundColor = originalConsoleForeground;
+};
+
+//a.ContinueWith(act);
+
+Breakfast.Do();
---
+++ AsyncFundamentals/TestAsyncFirst.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncFundamentals
+{
+    internal class TestAsyncFirst
+    {
+        // асинхронный успех
+        public async Task<T> DelayResult<T>(T result, TimeSpan delay)
+        {
+            await Task.Delay(delay);
+            return result;
+        }
+        public async Task<T> DelayException<T>(T result, TimeSpan delay)
+        {
+            await Task.Delay(delay);
+            throw new Exception("Error with doing something!");
+        }
+    }
+}
