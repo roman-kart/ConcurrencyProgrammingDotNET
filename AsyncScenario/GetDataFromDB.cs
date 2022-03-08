2fb87b1e5a031a4565331972008584fc7a81659e
roman-kart <romankartasheff@gmail.com>
2022-03-09 00:10:52 +0300

    ConcProgrNight

---
+++ AsyncScenario/GetDataFromDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncScenario
+{
+    internal class GetDataFromDB
+    {
+        public class Product
+        {
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+        }
+        public static async ValueTask<List<Product>> GetProducts()
+        {
+            var products = new List<Product>();
+            products.Add(new Product() { Name = "Chocolate", Price = 100 });
+            products.Add(new Product() { Name = "Milk", Price = 70 });
+            await Task.Delay(8000);
+            return new List<Product>(products);
+        }
+    }
+}
---
+++ AsyncScenario/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Hi, Mark!");
+
+var originalCursorPos = Console.GetCursorPosition();
+int waitingTime = 0;
+var prodResult = GetDataFromDB.GetProducts();
+
+while (!prodResult.IsCompleted)
+{
+    Console.SetCursorPosition(originalCursorPos.Left, originalCursorPos.Top);
+    Console.WriteLine($"You spended {waitingTime} seconds of your meaningless life :)");
+    Thread.Sleep(1000);
+    waitingTime++;
+}
+
+Console.SetCursorPosition(originalCursorPos.Left, originalCursorPos.Top + 1);
+Console.WriteLine("The data has been received successfuly!");
+foreach (var prod in prodResult.Result)
+{
+    Console.WriteLine($"{prod.Name}: {prod.Price}");
+}
