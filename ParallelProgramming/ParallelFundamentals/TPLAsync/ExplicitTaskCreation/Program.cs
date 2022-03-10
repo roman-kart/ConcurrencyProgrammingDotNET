0bdc9fe75cacfced9716bbff26fa5bd8c44fb00f
roman-kart <romankartasheff@gmail.com>
2022-03-11 00:37:02 +0300

    TPLAsyncNight

---
+++ ParallelProgramming/ParallelFundamentals/TPLAsync/ExplicitTaskCreation/Program.cs
@@ -0,0 +1,13 @@
+﻿Thread.CurrentThread.Name = "zaTASKanniy";
+
+// если необходимо контролировать выполнение задачи, можно явно создать ее при помощи конструктора класса Task
+Task taskA = new Task(() =>
+{
+    Console.WriteLine($"Task thread name: {Thread.CurrentThread.Name}");
+});
+
+taskA.Start();
+
+Console.WriteLine($"Main thread name: {Thread.CurrentThread.Name}");
+
+taskA.Wait();
---
+++ ParallelProgramming/ParallelFundamentals/TPLAsync/TPLAsyncTask/Program.cs
@@ -0,0 +1,28 @@
+﻿using RandomThings;
+
+var po = new ParallelOptions();
+po.MaxDegreeOfParallelism = Environment.ProcessorCount;
+
+string forSearch = "беб";
+
+List<Action> actions = new List<Action>();
+for (int i = 0; i < 100; i++)
+{
+    actions.Add(() =>
+    {
+        var words = new string[100];
+        words.InsertRandomStrings(10, 1040, 1073);
+
+        for (int j = 0; j < words.Length; j++)
+        {
+            if (words[j].Contains(forSearch, StringComparison.CurrentCultureIgnoreCase))
+            {
+                System.Console.WriteLine(words[j]);
+            }
+        }
+        global::System.Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId}");
+    });
+}
+
+Parallel.Invoke(po, actions.ToArray());
+Console.ReadKey();
