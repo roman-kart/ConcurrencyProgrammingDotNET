18cc91e9b3b137c5e69263e3b731ceee69a4c7bd
roman-kart <romankartasheff@gmail.com>
2022-03-17 22:49:46 +0300

    Chapter13Planning

---
+++ Planning/Planning/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Planning
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Task.Run - вынесение задачи из текущего потока в пул потоков.
+            // идеально для UI-приложений с долговыполняющимися операциями.
+            // НЕ ИСПОЛЬЗОВАТЬ В ASP.NET
+
+            var task = Task.Run(() =>
+            {
+                Console.WriteLine("Wait 2 seconds, please!");
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                Console.WriteLine("Thanks!");
+            });
+
+            Console.WriteLine("Before task's completing!");
+
+            
+            // выполнение кода при помощи планировщика задач
+
+            // TaskScheduler.Default // ставит работу в очередь пула потоков, используется по умолчанию, например, в
+            // Task.Run параллельном коде и коде потоков данных
+            
+            await Task.WhenAll(task);
+        }
+    }
+}
